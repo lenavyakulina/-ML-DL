28d5b39d32c5a275c1e729ca365ceea0fbede88e
Lena Vyakulina <lenavjakulina@mail.ru>
2022-09-08 13:49:41 +0300

    first commit

---
+++ generate.py
@@ -0,0 +1,99 @@
+import argparse
+import sys
+import random
+import pickle
+
+# ########################################
+# КОНСОЛЬНОЕ ВЗАИМОДЕЙСТВИЕ
+
+
+def deal_with_console():
+    """
+    Аргументы для запуска из консоли
+    :return: доступ к ним
+    """
+    parser = argparse.ArgumentParser(description='Генерация текст',
+                                     prog='generate', fromfile_prefix_chars='@')
+    parser.add_argument('--length', action='store',
+                        help='Длина генерируемой последовательности',
+                        default=False)
+    parser.add_argument('--model', action='store',
+                        help='Путь к файлу, из которого загружается модель',
+                        default=False)
+    parser.add_argument('--prefix', action='store', default=False,
+                        help='Начало предложения (одно слово). Если не указано, выбираем '
+                             'случайно из всех слов')
+    parser.add_argument('--output', action='store', default=False,
+                        help='Файл, в который будет записан результат. '
+                             'Если аргумент отсутствует, выводить в stdout.')
+
+    return parser.parse_args()
+
+# ########################################
+# ПРОВЕРКА НА ДОСТАТОЧНОСТЬ ИНФОРМАЦИИ ДЛЯ ГЕНЕРАЦИИ ТЕКСТА
+
+
+def check_console(args):
+    """
+    Функция, которая проверяет, указали ли в консоли модели для генерации
+    текста и длину для него
+    :param args: то, что ввели в консоли
+    :return: если проверка пройдена, вернет пару (модель - первое слово текста)
+    """
+    if not args.model:
+        print('Пожалуйста, укажите файл, из которого нужно загрузить модель')
+        sys.exit()
+
+    if not args.length:
+        print('Пожалуйста, укажите длину последовательности')
+        sys.exit()
+
+    f = open(args.model, 'rb')
+    model = pickle.load(f)
+    f.close()
+
+    startword = ""
+    if args.prefix:
+        startword = args.prefix
+    else:
+        startword = random.choice([k for k in model.keys()])
+    return model, startword
+
+
+# ########################################
+# ГЕНЕРАЦИЯ ТЕКСТА
+
+
+def generation_itself():
+    """
+    Функция, генерирующая текста по его длине и модели для него
+    """
+    args = deal_with_console()
+    model, word = check_console(args)
+
+    if args.output:
+        g = open(args.output, 'w')
+
+    ans = ''
+    for j in range(int(args.length)):
+        if args.output:
+            g.write(word + ' ')
+        else:
+            ans += word + ' '
+        loc = []
+        for elem in model[word]:
+            for num in range(elem[1]):
+                loc.append(elem[0])
+        word = random.choice(loc)
+
+    if args.output:
+        g.close()
+    else:
+        print(ans)
+
+
+# ########################################
+# CALL OF FUNCTIONS
+
+if __name__ == "__main__":
+    generation_itself()
---
+++ train.py
@@ -0,0 +1,130 @@
+import argparse
+import sys
+import re
+from collections import defaultdict
+import os
+import pickle
+
+r_alphabet = re.compile(u'[а-яА-Яa-zA-Z]+')
+
+# ########################################
+# КОНСОЛЬНОЕ ВЗАИМОДЕЙСТВИЕ
+
+
+def deal_with_console():
+    """
+    Аргументы для запуска из консоли
+    :return: доступ к ним
+    """
+    parser = argparse.ArgumentParser(description='Создание модели текста.',
+                                     prog='train', fromfile_prefix_chars='@')
+    parser.add_argument('--input', '--input-dir', action='store',
+                        help='Путь к директории, в которой лежит коллекция документов. '
+                        'Если данный аргумент не задан, считается, что тексты вводятся из stdin.',
+                        default=False)
+    parser.add_argument('--model', action='store',
+                        help='Путь к файлу, в который сохраняется модель.',
+                        default=False)
+
+    return parser.parse_args()
+
+# ########################################
+# GENERATOR OF LINES FROM STDIN
+
+
+def gen_lines_from_stdin(args):
+    """
+    Генератор строк из текста, введённого из stdin(консоли).
+    Приведение к lowercase.
+    :param args: то, что ввели в консоли
+    :return: строки
+    """
+    for line in sys.stdin:
+            yield line.lower()
+
+# ########################################
+# GENERATOR OF LINES FROM DIRECTORY
+
+def gen_lines_from_directory(args):
+    """
+    Генератор строк из файлов, которые находятся
+    в указанной пользователем директории.
+    Приведение к lowercase.
+    :param args: то, что ввели в консоли
+    :return: строки
+    """
+    directory = os.listdir(args.input)
+    for file in directory:
+        f = open(os.path.join(args.input, file), 'r')
+        for line in f:
+                yield line.lower()
+
+# ########################################
+# GENERATOR OF CLEAR WORDS
+
+def gen_tokens(lines):
+    """
+    :rtype: gen_tokens
+    Генератор, возвращает "очищенные" от неалфавитных символов слова из строк
+    """
+    for line in lines:
+        for token in r_alphabet.findall(line):
+            yield token
+
+# ########################################
+# GENERATOR OF WORD PAIRS
+
+
+def gen_bigrams(tokens):
+    """
+    :param tokens:
+    Генератор пар слов (предыдущее-последующее)
+    :return: пара
+    """
+    t1 = '$'
+    for t2 in tokens:
+        if t1 != '$':
+            yield t1, t2
+        t1 = t2
+
+# ########################################
+# MODEL CREATING
+
+
+def train_itself():
+    """
+    Функция, создающая словарь (model),
+    где ключ - слово,
+    а значение - пара (следующее слово + частота встречаемости пары)
+    :return:
+    """
+    args = deal_with_console()
+    if not args.model:
+        print('Пожалуйста, запустите программу ещё раз и укажите название файла, в который нужно сохранить модель')
+        sys.exit()
+
+    g = open(args.model, 'wb')
+    if args.input:
+        lines = gen_lines_from_directory(args)
+    else:
+        lines = gen_lines_from_stdin(args)
+
+    tokens = gen_tokens(lines)
+    b_grams = gen_bigrams(tokens)
+    bi = defaultdict(int)
+
+    for word0, word1 in b_grams:
+        bi[word0, word1] += 1
+
+    model = defaultdict(set)
+    for word0, word1 in bi.keys():
+        model[word0].add((word1, bi[word0, word1]))
+
+    pickle.dump(model, g)
+
+
+# ########################################
+# CALL OF FUNCTIONS
+
+if __name__ == '__main__':
+    train_itself()
